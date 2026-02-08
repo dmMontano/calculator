d5a7a92525a9e0c38fe56ca1fa44afda37f42642
Dannicah <dannicahmontano@gmail.com>
2026-02-08 16:12:24 +0800

    Update Calculator.cs

--- Calculator.cs
+++ Calculator.cs
@@ -1,12 +1,44 @@
+// Calculator by Dannicah Montano
+
 using System;
 
-public static class Calculator
+public class Calculator
 {
-    public static double? Calculate(int first, int second, string operation)
+     public static double? Calculate(int first, int second, string operation)
     {
-        //write your business logic in here
+        switch (operation)
+        {
+            case "+":
+                return first + second;
 
-        return null;
+            case "-":
+                return first - second;
+
+            case "*":
+                return first * second;
+
+            case "/":
+                if (second == 0)
+                {
+                    Console.WriteLine("Cannot divide by zero.");
+                    return null;
+                }
+                return (double)first / second;
+
+            case "%":
+                if (second == 0)
+                {
+                    Console.WriteLine("Cannot modulo by zero.");
+                    return null;
+                }
+                return first % second;
+
+            case "=":
+                Console.WriteLine("Program terminated.");
+                return null;
+
+            default:
+                return null;
+        }
     }
 }
-
